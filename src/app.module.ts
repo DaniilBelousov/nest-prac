fcd12720478a097814a2f48d32ecf8270f070e83
DaniilBelousov <chetrik10@gmail.com>
2022-08-26 22:31:07 +0300

    feat: added graphql errors formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
+import { RidesModule } from './rides/rides.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { typeDefs as scalarTypeDefs, resolvers as scalarResolvers } from 'graphql-scalars';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 
 @Module({
   imports: [
     UsersModule,
+    RidesModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
+      typeDefs: [...scalarTypeDefs],
+      resolvers: [scalarResolvers],
+      formatError: ({ message, extensions }: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message,
+          extensions: {
+            ...extensions
+          }
+        };
+        return graphQLFormattedError;
+      },
       installSubscriptionHandlers: true
     })
   ]
